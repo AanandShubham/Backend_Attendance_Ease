3a5cf710e0afd8f7a6176b453d9a0ca89ec7eda2
AanandShubham <www.subham.anand@gmail.com>
2025-09-16 20:14:57 +0530

    auth route done

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,5 +1,102 @@
+import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
+import { generateToken } from "../utils/generateToken.js"
 
-export const signup = async (req,res) => {
+export const signup = async (req, res) => {
+    try {
+        const { username, fullname, password, confirmPassword, securityKey } = req.body
+        console.log("signup request Got : details : ", req.body)
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "password and confirm password doesnot match" })
+        }
 
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res.status(409).json({ message: "User Already Available" })
+        }
+
+        // generating hashed password 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+
+        const newUser = await User.create({
+            username,
+            fullname,
+            password: hashedPassword,
+            securityKey
+        })
+
+        if (newUser) {
+            // generate token
+            const token = generateToken(newUser._id)
+            await newUser.save()
+            // setting cookies for api testing 
+            res.cookie('jwt', token, {
+                maxAge: 5 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                sameSite: "strict",
+            })
+            return res.status(201).json({ user: newUser, token })
+        }
+        else {
+            return res.status(400).json({ error: "Invalid User Data" })
+        }
+
+    } catch (error) {
+
+        console.log("--------------------------------------------------")
+        console.log("Error in auth controller Signup \nErron :", error)
+        console.log("--------------------------------------------------")
+        return res.status(500).json({ error: "Internal Server Error!!" })
+
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        const user = await User.findOne({ username })
+
+        const isPasswordTrue = await bcrypt.compare(password, user?.password || "")
+
+        if (!user && !isPasswordTrue) {
+            return res.status(500).json({ error: "User details are invalid !!" })
+        }
+
+        const token = generateToken(user._id)
+        // setting cookies for api testing 
+        res.cookie('jwt', token, {
+            maxAge: 5 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+        })
+
+        return res.status(200).json({ user, token })
+
+    } catch (error) {
+        console.log("--------------------------------------------------")
+        console.log("Error in auth controller Login \nErron :", error)
+        console.log("--------------------------------------------------")
+        return res.status(500).json({ error: "Internal Server Error!!" })
+    }
+}
+
+export const logout = async (req,res)=>{
+    try {
+        res.cookie("jwt", "", {
+            maxAge: 0
+        })
+
+        res.status(200).json({message:"Logout Successfully !!"})
+        
+    } catch (error) {
+        console.log("--------------------------------------------------")
+        console.log("Error in auth controller Logout \nErron :", error)
+        console.log("--------------------------------------------------")
+        return res.status(500).json({ error: "Internal Server Error!!" }) 
+    }
 }
 
---
+++ middleware/protectRoute.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model.js'
+
+export const protectRoute = async (req,res,next)=>{
+    try {
+        // const token = req.authentication 
+        const token = req.cookies.jwt
+
+        if(!token){
+            return res.status(401).json({error:"Un-authorized user, no token provided"})
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRETE)
+        
+        if(!decoded){
+            return res.status(401).json({error:"Un-authorized , Invalid token"})
+        }
+
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if(!user){
+            return res.status(401).json({error:"Un-authorized , User not found"})
+        }
+
+        req.user = user 
+
+        next()
+
+    } catch (error) {
+        console.log("--------------------------------------------------")
+        console.log("Error in Class controller AddClass \nErron :", error)
+        console.log("--------------------------------------------------")
+        return res.status(500).json({ error: "Internal Server Error!!" })
+    }
+}
